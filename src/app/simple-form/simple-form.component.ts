3a6360cb8d1c694b1a773b7b60cbc3c0fe0e20bf
Facundo Flores <flores.facundogabriel@gmail.com>
2017-07-18 13:14:28 -0300

    Add update posts with enter

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-    <input type="text" [(ngModel)]="message">
+    <input type="text" [(ngModel)]="message" (keydown)="onKeyDown($event)">
     <button (click)="update.emit({post:message})">Click me!</button>
   `,
   styles: []
@@ -14,6 +14,10 @@ export class SimpleFormComponent implements OnInit {
 
   constructor() { }
 
+  onKeyDown(key: KeyboardEvent) {
+    if (key.keyCode == 13)
+      this.update.emit({ post: this.message })
+  }
 
   ngOnInit() {
   }
